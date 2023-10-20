c7ea298428b5f9cf31d41c5bea8a11c83e18fb69
Lipe biondes <alefefelipebiondesmagalhaes@gmail.com>
2023-10-20 17:40:33 -0300

    Update conexao.php
    Colocando uma mensagem de erro quando não conseguimos conectar com o banco de dados

--- conexao.php
+++ conexao.php
@@ -3,5 +3,6 @@
 $conn = new mysqli("localhost", "root", "", "sistema_de_login");
 
 if ($conn->connect_error) {
+  header("Location: index.php?erro=Estamos com problemas técnicos, tente novamente mais tarde.");
   die("Conexão falhou: " . $conn->connect_error);
 }
