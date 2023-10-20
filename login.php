119d3212eed4b6b2112be29c4172575888b9f24f
Lipe biondes <alefefelipebiondesmagalhaes@gmail.com>
2023-10-20 19:47:32 -0300

    Error validation

--- login.php
+++ login.php
@@ -4,26 +4,30 @@ session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $email_do_usuario = $_POST["username"];
   $senha_do_usuario = $_POST["password"];
+  try {
+    include("conexao.php");
 
-  include("conexao.php");
+    // Procurar o usuário no banco
+    $stmt = $conn->prepare("SELECT id, nome, senha FROM usuario WHERE email = ?");
+    $stmt->bind_param("s", $email_do_usuario);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-  // Procurar o usuário no banco
-  $stmt = $conn->prepare("SELECT id, nome, senha FROM usuario WHERE email = ?");
-  $stmt->bind_param("s", $email_do_usuario);
-  $stmt->execute();
-  $result = $stmt->get_result();
-
-  if ($result->num_rows == 1) {
-    $row = $result->fetch_assoc();
-    $nome_do_usuario = $row["nome"];
-    $senha_criptografada = $row["senha"];
-    if (password_verify($senha_do_usuario, $senha_criptografada)) {
-      $_SESSION["email"] = $email_do_usuario;
-      $_SESSION["password"] = $senha_criptografada;
-      header("Location: home.php?nome=" .  $nome_do_usuario);
+    if ($result->num_rows == 1) {
+      $row = $result->fetch_assoc();
+      $nome_do_usuario = $row["nome"];
+      $senha_criptografada = $row["senha"];
+      if (password_verify($senha_do_usuario, $senha_criptografada)) {
+        $_SESSION["email"] = $email_do_usuario;
+        $_SESSION["password"] = $senha_criptografada;
+        header("Location: home.php?nome=" .  $nome_do_usuario);
+      }
+    } else {
+      unset($_SESSION['email']);
+      header("Location: index.html?error=Usuário não encontrado ou senha incorreta");
     }
-  } else {
-    unset($_SESSION['email']);
-    header("Location: index.html?error=Usuário não encontrado ou senha incorreta");
+  } catch (Exception $e) {
+    header("Location: index.html?error=Estamos com problemas técnicos, tente novamente mais tarde.");
+    session_destroy();
   }
 }
--- validar_codigo.php
+++ validar_codigo.php
@@ -4,33 +4,37 @@ $error_message = ""; // Inicialize a mensagem de erro como uma string vazia
 $codigo_recuperacao_usuario = $_GET["codigo"];
 $email_do_usuario = $_GET['email'];
 
-// Conecte-se ao banco de dados
-include("conexao.php");
+try {
+  // Conecte-se ao banco de dados
+  include("conexao.php");
 
-// Verifique se o usuario existe no banco de dados
-$query = "SELECT codigo_recuperacao FROM usuario WHERE email = ?";
-$stmt = $conn->prepare($query);
-$stmt->bind_param("s", $email_do_usuario);
-$stmt->execute();
-$result = $stmt->get_result();
+  // Verifique se o usuario existe no banco de dados
+  $query = "SELECT codigo_recuperacao FROM usuario WHERE email = ?";
+  $stmt = $conn->prepare($query);
+  $stmt->bind_param("s", $email_do_usuario);
+  $stmt->execute();
+  $result = $stmt->get_result();
 
-if ($result->num_rows === 0) {
-  // O usuario não foi encontrado no banco de dados
-  header("Location: recuperar_senha.php?error=Usuario não registrado. Por favor, verifique o e-mail fornecido.");
-} else {
-  // O e-mail foi encontrado, continue com a verificação do código
-  $row = $result->fetch_assoc();
-  $codigo_recuperado_banco = $row["codigo_recuperacao"];
-
-  if (password_verify($codigo_recuperacao_usuario, $codigo_recuperado_banco)) {
-    // O código inserido pelo usuário é válido então redireciono o usuário para a página de troca de senha com o email e o codigo na URL
-    header("Location: trocar_senha.php?email=" . $email_do_usuario . "&codigo=" . $codigo_recuperacao_usuario);
-    exit();
+  if ($result->num_rows === 0) {
+    // O usuario não foi encontrado no banco de dados
+    header("Location: recuperar_senha.php?error=Usuario não registrado. Por favor, verifique o e-mail fornecido.");
   } else {
-    // O código inserido é inválido, defina a mensagem de erro
-    header("Location: confirmar_codigo.php?error=Código de recuperação inválido. Tente novamente.&email=" . $email_do_usuario);
-  }
-}
+    // O e-mail foi encontrado, continue com a verificação do código
+    $row = $result->fetch_assoc();
+    $codigo_recuperado_banco = $row["codigo_recuperacao"];
 
-$stmt->close();
-$conn->close();
+    if (password_verify($codigo_recuperacao_usuario, $codigo_recuperado_banco)) {
+      // O código inserido pelo usuário é válido então redireciono o usuário para a página de troca de senha com o email e o codigo na URL
+      header("Location: trocar_senha.php?email=" . $email_do_usuario . "&codigo=" . $codigo_recuperacao_usuario);
+      exit();
+    } else {
+      // O código inserido é inválido, defina a mensagem de erro
+      header("Location: confirmar_codigo.php?error=Código de recuperação inválido. Tente novamente.&email=" . $email_do_usuario);
+    }
+  }
+
+  $stmt->close();
+  $conn->close();
+} catch (Exception $e) {
+  header("Location: index.html?error=Estamos com problemas técnicos, tente novamente mais tarde.");
+}
